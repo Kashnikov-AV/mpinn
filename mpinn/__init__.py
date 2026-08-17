eee7cf8ab58215e46a77d00513a2411a77ed455d
Kashnikov-AV <egkashnikov@gmail.com>
2026-08-17 19:33:55 +0300

    1111

---
+++ mpinn/__init__.py
@@ -0,0 +1,21 @@
+from . import analytic
+from . import bc
+from . import config
+from . import geom
+from . import multidomain
+from . import pde
+from . import pinn_core
+from . import plotting
+from . import runner
+
+__all__ = [
+    'analytic',
+    'bc',
+    'config',
+    'geom',
+    'multidomain',
+    'pde',
+    'pinn_core',
+    'plotting',
+    'runner',
+]
